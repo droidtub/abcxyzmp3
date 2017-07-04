8e049ac7814c26c7bbf27198313b949a1ad2c866
hanhnghh <hannh.nghh@gmail.com>
2017-07-05 00:46:18 +0700

    update onesignal

--- app/src/main/java/com/musicdownloader/mp3downloader/Mp3Application.java
+++ app/src/main/java/com/musicdownloader/mp3downloader/Mp3Application.java
@@ -1,6 +1,9 @@
 package com.musicdownloader.mp3downloader;
 
 import android.app.Application;
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.onesignal.OSNotification;
@@ -44,19 +47,10 @@ public class Mp3Application extends Application {
             if (actionType == OSNotificationAction.ActionType.ActionTaken)
                 Log.i("OneSignalExample", "Button pressed with id: " + result.action.actionID);
 
-            // The following can be used to open an Activity of your choice.
-            // Replace - getApplicationContext() - with any Android Context.
-            // Intent intent = new Intent(getApplicationContext(), YourActivity.class);
-            // intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
-            // startActivity(intent);
-
-            // Add the following to your AndroidManifest.xml to prevent the launching of your main Activity
-            //   if you are calling startActivity above.
-     /*
-        <application ...>
-          <meta-data android:name="com.onesignal.NotificationOpened.DEFAULT" android:value="DISABLE" />
-        </application>
-     */
+            String url = (PreferenceManager.getDefaultSharedPreferences(Mp3Application.this)).getString(getString(R.string.update_link_key), "");
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 
